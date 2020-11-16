e31cf0af7b7c448d183d029156105baa7abd5de7
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-11-16 21:42:43 +0800

    代码生成器创建代码

---
+++ guli_teacher/src/main/java/com/guli/teacher/service/impl/EduTeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.guli.teacher.service.impl;
+
+import com.guli.teacher.entity.EduTeacher;
+import com.guli.teacher.mapper.EduTeacherMapper;
+import com.guli.teacher.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author guli
+ * @since 2020-11-16
+ */
+@Service
+public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
+
+}
