5025f56c4f9b72848ccbd5d965303b3e51a0e827
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-11-16 14:40:37 +0800

    create project 创建项目

---
+++ src/test/java/com/guli/guli_parent/GuliParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guli.guli_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
